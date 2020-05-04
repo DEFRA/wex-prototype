2d1c45b07a8aaa21597b6b50171ca009fff1d688
Emi Carmichael <44699982+emiliabeth@users.noreply.github.com>
2020-05-04 15:13:18 +0100

    Tweaks to all WCR magic link screens from team feedback

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register your waste exemptions',
+  serviceName: 'Register as a waste carrier',
 
   // Default port that prototype runs on
   port: '3000',
